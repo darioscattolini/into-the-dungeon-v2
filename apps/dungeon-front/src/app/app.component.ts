19855805fc35114f01804c87f4b4f082845a908a
Darío Scattolini <dario.scattolini@gmail.com>
2021-07-27 23:59:13 +0200

    initial commit

---
+++ apps/dungeon-front/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'into-the-dungeon-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'dungeon-front';
+}
