659ab558862c34d447d9c03ab675a0e355f062a1
Darío Scattolini <dario.scattolini@gmail.com>
2021-07-30 13:54:03 +0200

    add testing utility functions

---
+++ test-utilities.ts
@@ -0,0 +1,35 @@
+type ConcreteConstructor = new(...args: any[]) => {};
+type AbstractConstructor = abstract new(...args: any[]) => {};
+type Constructor = ConcreteConstructor | AbstractConstructor;
+
+/**
+ * Builds a random integer between 0 and 10000. This value can be used for
+ * mocking.
+ * 
+ * @returns number
+ */
+declare function randomInteger(): number;
+
+global.randomInteger = () => Math.round(Math.random() * 10000);
+
+/**
+ * Builds an extension of the provided class with an additional property called
+ * `uniqueId` of type `Symbol`, which uniquely identifies a class instance. 
+ * This can help create mocks that don't incur in false positives in matchers 
+ * that check for equality rather than identity of objects. 
+ * 
+ * Example:
+ * 
+ * `const mockInstance = new (Unique(MockUser))();`
+ * 
+ * @param BaseClass Constructor
+ * @returns ConcreteConstructor
+ */
+declare function Unique(BaseClass: Constructor): ConcreteConstructor;
+
+global.Unique = 
+  <Base extends Constructor>(BaseClass: Base): ConcreteConstructor => {
+    return class extends BaseClass {
+      public readonly mockId = Symbol();
+    };
+  }
