c1da6f23fcd611d93ab499c9cee9779c6f70fcd3
Darío Scattolini <dario.scattolini@gmail.com>
2021-07-31 03:11:10 +0200

    feat<util-<ommon>: add staticImplements decorator

--- libs/util-common/src/lib/util-common.ts
+++ libs/util-common/src/lib/util-common.ts
@@ -1,3 +1,11 @@
-export function utilCommon(): string {
-  return 'util-common';
+/**
+ * Decorator used to specify the static interface a class must implement.
+ * 
+ * Example:
+ * `@staticImplements<Singleton>() class MyService()`
+ * 
+ * @returns function
+ */
+export function staticImplements<T>() {
+  return <U extends T>(constructor: U) => constructor;
 }
