5295a3d0bdfffe0f1f834c0d670f6dedaefd9589
Darío Scattolini <dario.scattolini@gmail.com>
2021-08-05 15:17:01 +0200

    feat<test-setup>: add custom Jest matcher toHaveNoRepeatedMembers

--- apps/dungeon-front/src/test-setup.ts
+++ apps/dungeon-front/src/test-setup.ts
@@ -1,2 +1,35 @@
 import 'jest-preset-angular/setup-jest';
 import 'jest-extended';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace jest {
+    interface Matchers<R> {
+      /** 
+       * Checks that an array does not have repeated members. For non primitive 
+       * values, it checks only for reference identity (different copies of the
+       * same object allowed)
+       */
+      toHaveNoRepeatedMembers(): R;
+    }
+  }
+}
+
+expect.extend({
+  toHaveNoRepeatedMembers(received) {
+    const pass = Array.isArray(received) 
+      && new Set(received).size === received.length;
+
+    if (pass) {
+      return {
+        message: () => `expected [${received}] array has no repeated members`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () => `expected [${received}] array has repeated members`,
+        pass: false,
+      };
+    }
+  },
+});
