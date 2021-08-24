8438e8c914a73831574b8724744c35ea7e81513f
Darío Scattolini <dario.scattolini@gmail.com>
2021-08-24 22:05:53 +0200

    feat<front-models-Monster>: implement Monster last features with corresponding testing suite

---
+++ apps/dungeon-front/src/app/models/monster/monster.spec.ts
@@ -0,0 +1,39 @@
+import { Monster, AnyMonster } from './monster';
+import { MonsterType } from './monster-type';
+import { pickRandomMonsterTypes } from '../test-doubles';
+import { randomInteger } from '@into-the-dungeon/util-testing';
+
+describe('Monster', () => {
+  let monster: AnyMonster;
+  let typeDummy: MonsterType;
+  let damageDummy: number;
+
+  beforeEach(() => {
+    [typeDummy] = pickRandomMonsterTypes(1);
+    damageDummy = randomInteger(10);
+    monster = new Monster(typeDummy, damageDummy);
+  });
+
+  test('it is created', () => {
+    expect(monster).toBeTruthy();
+  });
+
+  test('it is of type passed as constructor parameter', () => {
+    expect(monster.type).toBe(typeDummy);
+  });
+
+  test('it has damage passed as constructor parameter', () => {
+    expect(monster.damage).toBe(damageDummy);
+  });
+
+  test.each([
+    -6, -2, -1, 0, 1, 2, 4
+  ])('it throws error for negative damage (%i)', damageDummy => {
+    if (damageDummy < 0) {
+      expect(() => { new Monster(typeDummy, damageDummy); })
+        .toThrowError('Damage value must be 0 or positive.');
+    } else {
+      expect(() => { new Monster(typeDummy, damageDummy); }).not.toThrowError();
+    }
+  });
+});
--- apps/dungeon-front/src/app/models/monster/monster.ts
+++ apps/dungeon-front/src/app/models/monster/monster.ts
@@ -5,6 +5,7 @@ export class Monster<T extends MonsterType> {
   public readonly damage: number;
 
   constructor(type: T, damage: number) {
+    if (damage < 0) throw new Error('Damage value must be 0 or positive.');
     this.type = type;
     this.damage = damage;
   }
