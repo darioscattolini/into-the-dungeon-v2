793adf07b627d55e324c7ca646774bd389597929
Darío Scattolini <dario.scattolini@gmail.com>
2021-09-06 13:49:37 +0200

    fix<front-BiddingStateComponent>: implement AfterContentChecked hook to trigger change detection

--- apps/dungeon-front/src/app/one-device/components/bidding-state/bidding-state.component.ts
+++ apps/dungeon-front/src/app/one-device/components/bidding-state/bidding-state.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { 
+  AfterContentChecked, ChangeDetectorRef, Component, Input, ViewChild 
+} from '@angular/core';
 import { SwiperComponent } from 'swiper/angular';
 import SwiperCore, { Pagination } from "swiper";
 import { BiddingStateViewData } from '../../../models/models';
@@ -10,7 +12,7 @@ SwiperCore.use([Pagination]);
   templateUrl: './bidding-state.component.html',
   styleUrls: ['./bidding-state.component.scss']
 })
-export class BiddingStateComponent {
+export class BiddingStateComponent implements AfterContentChecked {
   @Input() dungeon!: BiddingStateViewData['dungeon'];
   @Input() hero!: BiddingStateViewData['hero'];
   @Input() remainingMonsters!: BiddingStateViewData['remainingMonsters'];
@@ -19,12 +21,18 @@ export class BiddingStateComponent {
   @ViewChild('equipmentSwiper') equipmemtSwiper?: SwiperComponent;
   @ViewChild('monsterSwiper') monsterSwiper?: SwiperComponent;
 
+  constructor(private changeDetector: ChangeDetectorRef) {}
+
   public nextEquipment(): void {
     this.equipmemtSwiper?.swiperRef.slideNext()
   }
 
   public nextMonster(): void {
     this.monsterSwiper?.swiperRef.slideNext()
+  }
+
+  public ngAfterContentChecked() {
+    this.changeDetector.detectChanges();
   }
 
   public previousEquipment(): void {
