8c1a1d85d268ee60e112a405e0d23139fca019ca
Darío Scattolini <dario.scattolini@gmail.com>
2021-09-15 07:52:09 +0200

    test<front>: implement ngMocks resets in test-setup

--- apps/dungeon-front/src/test-setup.ts
+++ apps/dungeon-front/src/test-setup.ts
@@ -1,20 +1,19 @@
 import 'jest-preset-angular/setup-jest';
 import 'jest-extended';
-import { ngMocks } from 'ng-mocks';
+import { MockInstance, ngMocks } from 'ng-mocks';
 
 // ng-mocks setup
   // all methods, getters and setters in mocks are spyied by default
 ngMocks.autoSpy('jest');
 
   // resets customized mock behaviour on afterEach and afterAll events
-/* Currently not working
 declare const jasmine: any;
 jasmine.getEnv().addReporter({
   specDone: MockInstance.restore,
   specStarted: MockInstance.remember,
   suiteDone: MockInstance.restore,
   suiteStarted: MockInstance.remember
-});*/
+});
 
 // Jest extension
 
